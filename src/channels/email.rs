b122b992c022f0af710c78eaf3d9572606a8179d
Peppi Littera <giuseppe.littera@gmail.com>
2026-03-03 00:57:47 +0100

    Auto-commit 1772495867

--- src/channels/email.rs
+++ src/channels/email.rs
@@ -810,6 +810,7 @@ mod tests {
             password: "test".to_string(),
             poll_interval_secs: 30,
             allow_from: Vec::new(),
+            toolset: None,
         };
 
         // With subject
